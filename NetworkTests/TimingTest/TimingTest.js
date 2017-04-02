a93d5342b6515ff78cffae86c4c949f6c479b6b1
Can Alper <calper@autoreg-094317.dyn.wpi.edu>
2017-04-02 15:33:34 -0400

    Phase 2 with network tests

--- NetworkTests/TimingTest/TimingTest.js
+++ NetworkTests/TimingTest/TimingTest.js
@@ -1,24 +1,56 @@
+// REQUIRED LIBS
 net = require('net');
-url = require('url');
 dns = require('dns');
-http = require('http');
-request = require('request');
 
+// Readline module
 var readline = require('readline');
 var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
 });
 
+
+// Recursive async function to test network connect time
+function testConn(address, count, max, total){
+  // Create a client and start a timer.
+  var client = new net.Socket();
+  var start = new Date();
+
+  // Connect the client
+  client.connect(80, address, function() {
+    // Once connected end the timer.
+    var end = new Date();
+    // BRUTALLY DESTROY THE CLIENT!!!
+    client.destroy();
+    // Calculate elapsed time
+    var elapsed = end.getTime() - start.getTime() ;
+    // Log the result
+    console.log(count + ": Connected within: " + elapsed + " ms");
+    // Count until max is reached by count
+    if(count < max){
+      // Recurse with count + 1
+      testConn(address, count+1, max, total+elapsed);
+    } else {
+      // Ended so give avarage
+      console.log("Avg time: " + total/max + " ms");
+    }
+  });
+
+}
+
 rl.question('Please enter the server address: ', (answer) => {
-    // TODO: Log the answer in a database
+  const options = {
+    family: 4,
+    hints: dns.ADDRCONFIG | dns.V4MAPPED,
+  };
 
-		request(answer, function (error, response, body) {
-		  console.log('error:', error); // Print the error if one occurred
-		  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		  console.log('body:', body); // Print the HTML for the Google homepage.
-		});
+  // Resolve DNS
+  dns.lookup(answer, options, (err, address, family) => {
+    console.log("Resolved adress is: " + address);
+    console.log("Testing:");
+    testConn(address, 0, 10, 0);
+  })
 
-    rl.close();
+  rl.close();
 });
